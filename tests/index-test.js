bc801e889b3ecdd37981f2fe9ed63e5811b6e983
Swizec Teller <swizec@swizec.com>
2019-01-09 22:29:28 -0800

    Port implementation from markdown-to-tweet repo

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,9 +1,34 @@
-import expect from 'expect'
+import expect from "expect";
+import remark from 'remark'
 
-import message from 'src/index'
+import utf8 from "src/index";
 
-describe('Module template', () => {
-  it('displays a welcome message', () => {
-    expect(message).toContain('Welcome to remark-utf8')
-  })
-})
+describe("italic", () => {
+    it("converts _italic_ into utf8 italics", done => {
+        remark().use(utf8).process("_italic_", function (err, output) {
+            const result = output.contents;
+            expect(result).toEqual("𝘪𝘵𝘢𝘭𝘪𝘤\n");
+            done();
+        });
+    });
+});
+
+describe("bold", () => {
+    it("converts **bold** into utf8 bolds", done => {
+        remark().use(utf8).process("**bold**", function (err, output) {
+            const result = output.contents;
+            expect(result).toEqual("𝗯𝗼𝗹𝗱\n");
+            done();
+        })
+    })
+});
+
+describe("code", () => {
+    it("converts `code` into monospace font", done => {
+        remark().use(utf8).process("`code`", function (err, output) {
+            const result = output.contents;
+            expect(result).toEqual("𝚌𝚘𝚍𝚎\n");
+            done();
+        })
+    })
+});
